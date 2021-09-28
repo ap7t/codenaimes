284d76015998f8edba8112ee977122e5faa4b7d8
ap7t <adampurcell7t@gmail.com>
2021-09-28 11:37:40 +0100

    src/lib wasn't in git

---
+++ frontend/src/lib/AgentCard.svelte
@@ -0,0 +1,27 @@
+<script>
+	export let name;
+	export let num;
+	export let colour;
+
+	function showColour() {
+		console.log(`card-${num}`);
+		let card = document.getElementById(`card-${num}`);
+		card.style.backgroundColor = colour;
+	}
+</script>
+
+<div id="card-{num}" style="background-color: {colour}" on:click={showColour}>
+	{name}
+</div>
+
+<style>
+	div {
+		text-transform: uppercase;
+		margin: 1em;
+		padding-top: 2em;
+		border: 1px solid black;
+		height: 100px;
+		width: 100px;
+		text-align: center;
+	}
+</style>
---
+++ frontend/src/lib/GameBoard.svelte
@@ -0,0 +1,57 @@
+<script>
+	import AgentCard from "./AgentCard.svelte";
+
+	// export let agents;
+
+	// dummy data
+	let agents = [
+		{ name: "resonant", colour: "blue" },
+		{ name: "tangy", colour: "yellow" },
+		{ name: "moldy", colour: "yellow" },
+		{ name: "powerful", colour: "yellow" },
+		{ name: "dad", colour: "blue" },
+		{ name: "sweater", colour: "blue" },
+		{ name: "experience", colour: "yellow" },
+		{ name: "tart", colour: "blue" },
+		{ name: "island", colour: "red" },
+		{ name: "sneaky", colour: "yellow" },
+		{ name: "church", colour: "yellow" },
+		{ name: "modern", colour: "red" },
+		{ name: "pipe", colour: "blue" },
+		{ name: "hospital", colour: "red" },
+		{ name: "examine", colour: "yellow" },
+		{ name: "efficient", colour: "yellow" },
+		{ name: "twist", colour: "blue" },
+		{ name: "thank", colour: "blue" },
+		{ name: "scrub", colour: "red" },
+		{ name: "crook", colour: "yellow" },
+		{ name: "clover", colour: "red" },
+		{ name: "assorted", colour: "yellow" },
+		{ name: "foregoing", colour: "red" },
+		{ name: "embarrassed", colour: "red" },
+		{ name: "glow", colour: "red" }
+	];
+
+	export let spymaster = false;
+</script>
+
+<div class="agentsGrid">
+	{#each agents as agent, i}
+		{#if spymaster}
+			<AgentCard name={agent.name} colour={agent.colour} num={i} />
+		{:else}
+			<AgentCard name={agent.name} num={i} />
+		{/if}
+	{/each}
+</div>
+
+<style>
+	.agentsGrid {
+		width: 600px;
+		display: grid;
+		grid-gap: 75px;
+		grid-template-columns: repeat(5, 100px);
+		grid-template-rows: repeat(5, 100px);
+		grid-auto-flow: column;
+	}
+</style>
